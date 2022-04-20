88fe4c83d5bd92fb04b7e9736fb1be06a252b064
Bram Hammer <bram.h@fullstak.nl>
2022-04-20 23:33:25 +0200

    V0.9.2 small fix for auto  updater

--- index.tsx
+++ index.tsx
@@ -1,21 +1,34 @@
 import app from "./splinterlands/index";
+import path from 'path'
+const fs   = require('fs');
 const AutoGitUpdate = require('auto-git-update');
-
-const config = {
-	repository: 'https://github.com/breakerh/hammer-splinterlands-bot',
-	fromReleases: true,
-	tempLocation: 'tmp/',
-	executeOnComplete: 'start.bat',
-	exitOnComplete: false
-}
-const updater = new AutoGitUpdate(config);
-updater.compareVersions().then(response => {
-	if(response.upToDate){
-		const APP = new app();
-		APP.isReady.then(async () => {await APP.versionCheck()}).then(() => {
-			APP.loopAccounts();
-		});
-	}else{
-		updater.forceUpdate();
+const tmpLocation  = path.resolve(__dirname,'../splinterbot-updater');
+try {
+	fs.accessSync(tmpLocation, fs.constants.R_OK | fs.constants.W_OK);
+	const config = {
+		repository: 'https://github.com/breakerh/hammer-splinterlands-bot',
+		fromReleases: true,
+		tempLocation: tmpLocation,
+		executeOnComplete: 'start.bat',
+		exitOnComplete: false
 	}
-});
+
+	const updater = new AutoGitUpdate(config);
+	updater.compareVersions().then(response => {
+		if(response.upToDate){
+			const APP = new app();
+			APP.isReady.then(async () => {await APP.versionCheck()}).then(() => {
+				APP.loopAccounts();
+			});
+		}else{
+			updater.forceUpdate();
+		}
+	});
+}catch(e){
+	console.error('Couldn\'t make update folder. Please create '+tmpLocation);
+
+	/*const APP = new app();
+	APP.isReady.then(async () => {await APP.versionCheck()}).then(() => {
+		APP.loopAccounts();
+	});*/
+}
